bc114729a95a4a5dc38bf24475fd71b0fbb6085a
Zach <zachstruck@gmail.com>
2019-01-03 21:13:19 +0700

    Read file

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
 #[macro_use]
 extern crate clap;
 use clap::App;
+use std::fs;
 
 fn main() {
     let yaml = load_yaml!("cli.yml");
-    let _ = App::from_yaml(yaml).get_matches();
+    let matches = App::from_yaml(yaml).get_matches();
+
+    let vcxproj_filename = matches.value_of("VCXPROJ").unwrap();
+
+    let vcxproj = fs::read_to_string(vcxproj_filename).unwrap();
+
+    println!("{}", vcxproj);
 }
