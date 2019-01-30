98dddc98a4d557b8f72a2e87fa0bd4481e349f40
Zach <zachstruck@gmail.com>
2019-01-30 12:46:24 +0700

    Fix parsing multiple expressions at the same level

--- src/condition.rs
+++ src/condition.rs
@@ -6,16 +6,17 @@ use std::path::Path;
 pub struct ConditionParser;
 
 enum Term<'a> {
+    Exprs(Vec<Term<'a>>),
     Exists(&'a str),
     FinalSlash(&'a str),
     Eq(&'a str, &'a str),
     Ne(&'a str, &'a str),
     Not(Box<Term<'a>>),
-    And(Box<Term<'a>>, Box<Term<'a>>),
-    Or(Box<Term<'a>>, Box<Term<'a>>),
+    And(Box<Term<'a>>),
+    Or(Box<Term<'a>>),
 }
 
-fn eval_condition(s: &str) -> bool {
+pub fn eval_condition(s: &str) -> bool {
     let mut parse_result = ConditionParser::parse(Rule::main, s).unwrap();
     let parse_result = parse_result.next().unwrap();
 
@@ -25,7 +26,7 @@ fn eval_condition(s: &str) -> bool {
 
     fn parse_start(pair: Pair<Rule>) -> Term {
         match pair.as_rule() {
-            Rule::expr => parse_expr(pair),
+            Rule::exprs => parse_exprs(pair),
             _ => unreachable!(),
         }
     }
@@ -51,7 +52,7 @@ fn eval_condition(s: &str) -> bool {
     fn parse_group(pair: Pair<Rule>) -> Term {
         let inner_rule = pair.into_inner().next().unwrap();
         match inner_rule.as_rule() {
-            Rule::expr => parse_expr(inner_rule),
+            Rule::exprs => parse_exprs(inner_rule),
             _ => unreachable!(),
         }
     }
@@ -65,10 +66,20 @@ fn eval_condition(s: &str) -> bool {
             Rule::eq => parse_eq(inner_rule),
             Rule::ne => parse_ne(inner_rule),
             Rule::not => parse_not(inner_rule),
-            Rule::and => parse_and(inner_rule),
-            Rule::or => parse_or(inner_rule),
             _ => unreachable!(),
         }
+    }
+
+    fn parse_exprs(pair: Pair<Rule>) -> Term {
+        Term::Exprs(
+            pair.into_inner()
+                .map(|pair| match pair.as_rule() {
+                    Rule::and => parse_and(pair),
+                    Rule::or => parse_or(pair),
+                    _ => parse_expr(pair),
+                })
+                .collect(),
+        )
     }
 
     fn parse_operand(pair: Pair<Rule>) -> Term {
@@ -77,6 +88,7 @@ fn eval_condition(s: &str) -> bool {
             Rule::group => parse_group(inner_rule),
             Rule::exists => parse_exists(inner_rule),
             Rule::final_slash => parse_final_slash(inner_rule),
+            Rule::not => parse_not(inner_rule),
             Rule::eq => parse_eq(inner_rule),
             Rule::ne => parse_ne(inner_rule),
             _ => unreachable!(),
@@ -143,45 +155,46 @@ fn eval_condition(s: &str) -> bool {
 
     fn parse_and(pair: Pair<Rule>) -> Term {
         let mut data = pair.into_inner();
-        let lhs_pair = data.next().unwrap();
-        let lhs = match lhs_pair.as_rule() {
-            Rule::operand => parse_operand(lhs_pair),
+        let pair = data.next().unwrap();
+        let value = match pair.as_rule() {
+            Rule::operand => parse_operand(pair),
             _ => unimplemented!(),
         };
-        let rhs_pair = data.next().unwrap();
-        let rhs = match rhs_pair.as_rule() {
-            Rule::operand => parse_operand(rhs_pair),
-            _ => unimplemented!(),
-        };
-        Term::And(Box::new(lhs), Box::new(rhs))
+        Term::And(Box::new(value))
     }
 
     fn parse_or(pair: Pair<Rule>) -> Term {
         let mut data = pair.into_inner();
-        let lhs_pair = data.next().unwrap();
-        let lhs = match lhs_pair.as_rule() {
-            Rule::operand => parse_operand(lhs_pair),
+        let pair = data.next().unwrap();
+        let value = match pair.as_rule() {
+            Rule::operand => parse_operand(pair),
             _ => unimplemented!(),
         };
-        let rhs_pair = data.next().unwrap();
-        let rhs = match rhs_pair.as_rule() {
-            Rule::operand => parse_operand(rhs_pair),
-            _ => unimplemented!(),
-        };
-        Term::Or(Box::new(lhs), Box::new(rhs))
+        Term::Or(Box::new(value))
     }
 
     let ast = parse_start(main_result);
 
     fn process<'a>(term: &Term) -> bool {
         match term {
+            Term::Exprs(exprs) => {
+                let mut b = process(&exprs[0]);
+                for i in 1..exprs.len() {
+                    b = match &exprs[i] {
+                        Term::And(rhs) => b && process(&rhs),
+                        Term::Or(rhs) => b || process(&rhs),
+                        _ => unreachable!(),
+                    }
+                }
+                b
+            }
             Term::Exists(path) => Path::new(path).exists(),
             Term::FinalSlash(s) => s.ends_with('\\') || s.ends_with('/'),
             Term::Eq(lhs, rhs) => lhs == rhs,
             Term::Ne(lhs, rhs) => lhs != rhs,
             Term::Not(value) => !process(&value),
-            Term::And(lhs, rhs) => process(&lhs) && process(&rhs),
-            Term::Or(lhs, rhs) => process(&lhs) || process(&rhs),
+            Term::And(rhs) => process(&rhs),
+            Term::Or(rhs) => process(&rhs),
         }
     }
 
@@ -191,6 +204,19 @@ fn eval_condition(s: &str) -> bool {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_eval() {
+        assert_eq!(eval_condition("s == s and s == s"), true);
+        assert_eq!(eval_condition("s != s and s == s"), false);
+        assert_eq!(eval_condition("s == s or s == s or s == s"), true);
+        assert_eq!(eval_condition("'' != '' and !exists('')"), false);
+    }
+
+    #[test]
+    fn test_exists() {
+        assert_eq!(eval_condition("!exists('')"), true);
+    }
 
     #[test]
     fn test_trailing_slash() {
@@ -216,6 +242,7 @@ mod tests {
     fn test_ne() {
         assert_eq!(eval_condition("s != s"), false);
         assert_eq!(eval_condition("s != q"), true);
+        assert_eq!(eval_condition("'' != ''"), false);
     }
 
     #[test]
